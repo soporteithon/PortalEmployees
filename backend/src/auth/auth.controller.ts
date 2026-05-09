4446ba18c9bc1dcff700189d255e8315e29055d9
Jorge Ariel Espinal <jespinalmontenegro@gmail.com>
2026-05-09 16:47:20 -0600

    Refactor: Limpieza de rutas de prueba y optimización de imports en AuthController

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,16 +1,12 @@
-import { Controller, Get, Post, Body, UseGuards, Req, Headers, SetMetadata, Patch, Param, ParseIntPipe } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, Post, Body, Patch, Param, ParseIntPipe } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-
-import { IncomingHttpHeaders } from 'http';
-
 import { AuthService } from './auth.service';
-import { RawHeaders, GetUser, Auth } from './decorators';
-import { RoleProtected } from './decorators/role-protected.decorator';
+import { GetUser, Auth } from './decorators';
+
 
 import { CreateUserDto, LoginUserDto, UpdateUserDto, ChangePasswordDto } from './dto';
 import { User } from './entities/user.entity';
-import { UserRoleGuard } from './guards/user-role.guard';
+
 import { ValidRoles } from './interfaces';
 
 @ApiTags('Auth')
@@ -69,56 +65,56 @@ export class AuthController {
   }
 
 
-  @Get('private')
-  @UseGuards(AuthGuard())
-  testingPrivateRoute(
-    @Req() request: Express.Request,
-    @GetUser() user: User,
-    @GetUser('email') userEmail: string,
+  // @Get('private')
+  // @UseGuards(AuthGuard())
+  // testingPrivateRoute(
+  //   @Req() request: Express.Request,
+  //   @GetUser() user: User,
+  //   @GetUser('email') userEmail: string,
 
-    @RawHeaders() rawHeaders: string[],
-    @Headers() headers: IncomingHttpHeaders,
-  ) {
+  //   @RawHeaders() rawHeaders: string[],
+  //   @Headers() headers: IncomingHttpHeaders,
+  // ) {
 
 
-    return {
-      ok: true,
-      message: 'Hola Mundo Private',
-      user,
-      userEmail,
-      rawHeaders,
-      headers
-    }
-  }
+  //   return {
+  //     ok: true,
+  //     message: 'Hola Mundo Private',
+  //     user,
+  //     userEmail,
+  //     rawHeaders,
+  //     headers
+  //   }
+  // }
 
 
   // @SetMetadata('roles', ['admin','super-user'])
 
-  @Get('private2')
-  @RoleProtected(ValidRoles.superUser, ValidRoles.admin)
-  @UseGuards(AuthGuard(), UserRoleGuard)
-  privateRoute2(
-    @GetUser() user: User
-  ) {
+  // @Get('private2')
+  // @RoleProtected(ValidRoles.superUser, ValidRoles.admin)
+  // @UseGuards(AuthGuard(), UserRoleGuard)
+  // privateRoute2(
+  //   @GetUser() user: User
+  // ) {
 
-    return {
-      ok: true,
-      user
-    }
-  }
+  //   return {
+  //     ok: true,
+  //     user
+  //   }
+  // }
 
 
-  @Get('private3')
-  @Auth(ValidRoles.admin)
-  privateRoute3(
-    @GetUser() user: User
-  ) {
+  // @Get('private3')
+  // @Auth(ValidRoles.admin)
+  // privateRoute3(
+  //   @GetUser() user: User
+  // ) {
 
-    return {
-      ok: true,
-      user
-    }
-  }
+  //   return {
+  //     ok: true,
+  //     user
+  //   }
+  // }
 
 
 
